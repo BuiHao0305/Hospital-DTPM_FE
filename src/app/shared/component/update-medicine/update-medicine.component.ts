2884497ceb6c5ac9ca4257c94567c9afedc3af67
Haobui0305 <haobui2512@gmail.com>
2024-12-03 14:54:19 +0700

    update get id

--- src/app/shared/component/update-medicine/update-medicine.component.ts
+++ src/app/shared/component/update-medicine/update-medicine.component.ts
@@ -1,50 +1,55 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-update-medicine',
-  templateUrl: './update-medicine.component.html',
-  styleUrls: ['./update-medicine.component.scss'],
+  selector: "app-update-medicine",
+  templateUrl: "./update-medicine.component.html",
+  styleUrls: ["./update-medicine.component.scss"],
   standalone: true,
-  imports: [FormsModule,CommonModule,ReactiveFormsModule]
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
 })
 export class UpdateMedicineComponent implements OnInit {
-
   showChildUpdate = false;
 
-  @Output() previewVisible = new EventEmitter<boolean>(); 
-  @Input() medicineId = ''; 
+  @Output() previewVisible = new EventEmitter<boolean>();
+  @Input() medicineId = "";
   blockFormClosing(event: MouseEvent) {
     event.stopPropagation();
   }
   loading = false;
   medicineForm!: FormGroup;
-  constructor( private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.medicineForm = this.fb.group({
-      name: ['', [Validators.required]],
-      price: [0, [Validators.required, Validators.min(999)]],  
-      description: ['', [Validators.required]],
+      name: ["", [Validators.required]],
+      price: [0, [Validators.required, Validators.min(999)]],
+      description: ["", [Validators.required]],
       quantity: [0, [Validators.required, Validators.min(1)]],
-      category: [0]
+      category: [0],
     });
   }
-  onSubmit(){
-    if (this.medicineId) {
-      console.log('Updating medicine with ID:', this.medicineId);
-    }
+  onSubmit() {
     if (this.medicineForm.valid) {
       const newMedicine = this.medicineForm.value;
-      console.log('New Medicine:', newMedicine);
-     
+      console.log("New Medicine:", newMedicine);
     } else {
-      console.error('Form is invalid');
+      console.error("Form is invalid");
+    }
+  }
+  ngOnChanges() {
+    if (this.medicineId) {
+      console.log('Updating medicine with ID:', this.medicineId);
     }
   }
   changeVisible() {
     this.previewVisible.emit(false);
   }
-
 }
